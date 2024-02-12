c9deb8beadfbff287fc0686ee6a92d5318066c36
Abhi Velaga <abhinav.velaga@gmail.com>
2024-02-12 15:16:42 -0600

    fonts work

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,17 @@
 import Navbar from './navbar'
 // import Footer from './footer'
+import localFont from 'next/font/local';
+
+const sukhumvitSet = localFont({
+  src: '../public/sukhumvit-set/SukhumvitSet-Bold.ttf',
+  variable: '--font-sukhumvitSet'
+})
  
 export default function Layout({ children }) {
   return (
     <>
-      <Navbar />
-      <main>{children}</main>
+      <Navbar className={sukhumvitSet.class}/>
+      <main className={sukhumvitSet.variable}>{children}</main>
       {/* <Footer /> */}
     </>
   )
