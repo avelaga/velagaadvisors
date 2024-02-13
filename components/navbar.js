508b39607c83cf4302bd4cc4b8cde91ec6bb05ed
Abhi Velaga <abhinav.velaga@gmail.com>
2024-02-13 13:14:31 -0600

    beta desktop done

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import Link from 'next/link'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import styles from "@/styles/Navbar.module.css";
 
 export default function Navbar() {
@@ -8,9 +9,9 @@ export default function Navbar() {
             <Link href="/"><Image src="/logo.png" width={200} height={0} style={{ width: '300px', height: 'auto' }} className={styles.logo}/></Link>
             
             <div className={styles.right}>
-                <Link href="/services" className={styles.link}>SERVICES</Link>
-                <Link href="/contact" className={styles.link}>CONTACT</Link>
-                <Link href="/about" className={styles.link}>ABOUT</Link>
+                <Link href="/services" className={usePathname() === '/services' ? `${styles.link} ${styles.active}` : styles.link}>SERVICES</Link>
+                <Link href="/about" className={usePathname() === '/about' ? `${styles.link} ${styles.active}` : styles.link}>ABOUT</Link>
+                <Link href="https://docs.google.com/forms/d/18oA11rmQZ94HzUDiNUoW-95oIC-rVxt0POIYccwZRT8/edit" target="blank" className={styles.link}>CONTACT</Link>
             </div>
 
         </div>
