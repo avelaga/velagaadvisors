0e1dbac0798afe6f243bf6070d577e4a05a56acd
Abhi Velaga <abhinav.velaga@gmail.com>
2024-02-22 13:53:51 -0600

    added abhi credit

--- components/footer.js
+++ components/footer.js
@@ -5,11 +5,14 @@ import privacyPolicy from '../public/privacyPolicy.pdf'
 
 export default function Footer() {
     return (
-        <div className={styles.footer}>
-            <div className={styles.item}>© 2024 Velaga Advisors LLC</div>
-            <a href={privacyPolicy} target="_blank" className={styles.item}>Privacy Policy</a>
-            {/* <div className={styles.link}>Privacy Policy</div> */}
-            <Link href="/disclaimer" className={`${styles.link} ${styles.item}`}>Disclaimer</Link>
+        <div>
+            <div className={styles.footer}>
+                <div className={styles.item}>© 2024 Velaga Advisors LLC</div>
+                <a href={privacyPolicy} target="_blank" className={styles.item}>Privacy Policy</a>
+                {/* <div className={styles.link}>Privacy Policy</div> */}
+                <Link href="/disclaimer" className={`${styles.link} ${styles.item}`}>Disclaimer</Link>
+            </div>
+            <a href="https://abhi.work" target="_blank" className={styles.abhi} >Designed and Developed by Abhi Velaga</a>
         </div>
     )
 }
