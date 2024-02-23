9bc02e63394c6444ec96630f4cf7c40cdc414557
Abhi Velaga <abhinav.velaga@gmail.com>
2024-02-23 14:40:58 -0600

    google analytics bug fix

--- pages/services.js
+++ pages/services.js
@@ -22,7 +22,7 @@ export default function Services() {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y0GC6KBE56"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
+          function gtag(){window.dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'G-Y0GC6KBE56');
