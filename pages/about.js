f9a2a12b822954de76a1186326cc0433cb1d0d69
Abhi Velaga <abhinav.velaga@gmail.com>
2025-02-09 16:02:35 -0600

    updated about page with kalyan pic and new text

--- pages/about.js
+++ pages/about.js
@@ -32,28 +32,46 @@ export default function About() {
             </Head>
             <main className={styles.about}>
                 <div className={styles.content}>
-                    <div className={styles.left}>
-                        <Image src="/krishna.jpg" width={200} height={200} style={{ width: '100%', height: 'auto' }} className={styles.krishna} />
-                        <div className={styles.photoCredit}>Photo by Abhi Velaga</div>
-                        <div className={styles.education}>
-                            <bold>MS</bold>, Computer Science, Kansas State University<br /><br />
-                            <bold>MBA</bold>, Cox School of Business,
-                            SMU<br /><br />
-                            Passed CFA Level I and Level II Examinations
+                    <div className={styles.row}>
+                        <div className={styles.left}>
+                            <Image src="/krishna.jpg" width={200} height={200} style={{ width: '100%', height: 'auto' }} className={styles.profilePic} />
+                            <div className={styles.photoCredit}>Photo by Abhi Velaga</div>
+                            <div className={styles.education}>
+                                <bold>MS</bold>, Computer Science, Kansas State University<br /><br />
+                                <bold>MBA</bold>, Cox School of Business,
+                                SMU<br /><br />
+                                Passed CFA Level I and Level II Examinations
+                            </div>
+                        </div>
+                        <div className={styles.right}>
+                            <div className={styles.header}>Krishna Velaga</div>
+                            <div className={styles.subheader}>Founder / Chief Investment Officer</div>
+                            <div className={styles.body}>
+                                I have deep passion for Investment Research, Tax Strategies, College Savings, Retirement Planning and other areas of personal finance. My journey into the world of investing began in the late 1990s with equities and derivatives. In pursuit of higher risk-adjusted returns and generating quality passive income, I have later expanded my focus to include alternative investments such as Commercial Real Estate, Private Equity, Private Credit, Hedge Funds, Structured Notes etc.
+                                <br /><br />
+                                I realized that many high net worth individuals busy in their professional careers could benefit from a trusted financial advisor who brings together knowledge of personal finance and taxes along with analytical and communication skills. I left a successful career in Information Technology in 2023 to pursue my passion and make a difference to others in these areas, thus leading to the founding of Velaga Advisors.
+                            </div>
                         </div>
                     </div>
-                    <div className={styles.right}>
-                        <div className={styles.header}>Krishna Velaga</div>
-                        <div className={styles.subheader}>Founder / Chief Investment Officer</div>
-                        <div className={styles.body}>
-                            I have always been passionate about investments and financial planning, and enjoyed sharing my knowledge to help family and friends on investments, college savings, tax planning and various other personal finance topics. I started investing in public equity markets in the late 1990s and expanded into alternative investments (commercial real estate, private equity, private credit, etc.) in 2016 to diversify my portfolio and build more reliable passive income streams. This helped me gain a lot of knowledge and insights into new areas and eventually helped me reach financial independence early.
-                            <br /><br />
-                            I realized that many high net worth individuals busy in their professional careers could benefit from a trusted financial advisor who brings together knowledge of personal finance and taxes along with analytical and communication skills. I retired from a successful career in Information Technology in 2023 to pursue my passion and make a difference to others in these areas, thus leading to the founding of Velaga Advisors.
-                            <br /><br />
-                            Whether you are a high earner in the wealth accumulation phase, a high net worth individual trying to further diversify and preserve wealth, or a young professional who has recently got into workforce and want to get on the right path for financial success, we are there to help.
-                            <br /><br />
-                            As a registered investment advisor, Velaga Advisors and its associates have a fiduciary duty to our clients to act in their best interests. We neither sell any insurance products nor do we receive any commissions or compensation other than the fee that our clients pay for our services.
 
+                    <div className={styles.row}>
+                        <div className={styles.left}>
+                            <Image src="/kalyan.jpg" width={200} height={200} style={{ width: '100%', height: 'auto' }} className={styles.profilePic} />
+                            <div className={styles.education}>
+                                <bold>MS</bold>, Electrical Engineering, Clemson University<br /><br />
+                                <bold>Certified Financial Planning</bold>, Texas A&M University<br /><br />
+                                (Est. Completion – May 2025)
+                            </div>
+                        </div>
+                        <div className={styles.right}>
+                            <div className={styles.header}>Kalyan Cherukuri</div>
+                            <div className={styles.subheader}>Chief Financial Planner</div>
+                            <div className={styles.body}>
+                                My passion for helping individuals, businesses, and non-profits make sound financial and investment decisions has led me to pursue the path of CERTIFIED FINANCIAL PLANNER™.  After 29 years working in the Electronics Industry analyzing various types of data to make profitable decisions with resounding success, I am looking forward to using the same data analytic skills as a Financial Advisor.  I believe that the best investment is one that positively impacts the future of others.
+                                <br /><br />
+                                As Chief Financial Planner at Velaga Advisors, my goal is to work with each of our valued clients in achieving the financial future they envision.
+
+                            </div>
                         </div>
                     </div>
                 </div>
